3b085f79d7a682abdc111ad463c5d4fed1eb8c1e
Jack Tepper <jatep2005@gmail.com>
2026-03-21 15:27:05 -0400

    Changes main so that it runs until exited

--- Versions/MK0/Firmware/main.py
+++ Versions/MK0/Firmware/main.py
@@ -3,15 +3,18 @@ from gestures import *
 def main():
     servos = setup()
 
-    print("Enter a keybind: \n")
-    print("Close Hand = a, Open Hand = b, Peace Sign = c")
-    button = input()
-    if button == 'a':
-        close_hand(servos)
-    elif button == 'b':
-        open_hand(servos)
-    elif button == 'c':
-        peace_sign(servos)
+    while True:
+        print("Enter a keybind: \n")
+        print("Close Hand = a, Open Hand = b, Peace Sign = c, Exit = x")
+        button = input()
+        if button == 'a':
+            close_hand(servos)
+        elif button == 'b':
+            open_hand(servos)
+        elif button == 'c':
+            peace_sign(servos)
+        elif button == 'x':
+            break
 
 # main_guard
 if __name__ == "__main__":
