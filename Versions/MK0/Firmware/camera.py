5ba665bbc36c6ad86146ccb9734d419952a28ec1
Jack Tepper <jatep2005@gmail.com>
2026-03-31 01:46:43 -0400

    Add camera.py with live preview feed

--- Versions/MK0/Firmware/camera.py
+++ Versions/MK0/Firmware/camera.py
@@ -1,1 +1,22 @@
+import cv2 as cv
+from picamera2 import Picamera2
 
+# Initializes the camera object, sets the camera configurations, passes the
+# configurations into the camera object, and starts the camera.
+picam = Picamera2()
+config = picam.create_preview_configuration({'size': (1920,1080), 'format': 'RGB888'})
+picam.configure(config)
+picam.start()
+
+# Runs infinitely capturing one frame at a time with said frame being passed
+# into 'imshow' which displays the frames in a window called 'Live Feed'.
+while True:
+   frame = picam.capture_array()
+   
+   cv.imshow('Live Feed', frame)
+
+    # Shows the next frame after 20ms and closes the 'Live Feed' if the
+    # keybind 'd' is pressed.
+   if cv.waitKey(20) & 0xFF==ord('d'):
+      break
+   
