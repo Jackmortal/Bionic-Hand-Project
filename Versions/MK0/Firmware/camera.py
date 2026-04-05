cd4faa6488e2560aa7f490ec90ee9a538536258f
Jack Tepper <jatep2005@gmail.com>
2026-04-04 20:35:26 -0400

    Add generator to frame_stream

--- Versions/MK0/Firmware/camera.py
+++ Versions/MK0/Firmware/camera.py
@@ -9,18 +9,18 @@ def setup():
    config = picam.create_preview_configuration({'size': (640,480), 'format': 'RGB888'}, transform=Transform(hflip=1))
    picam.configure(config)
    picam.start()
-   run_camera()
+   run_camera(picam)
 
 # Runs infinitely capturing one frame at a time with said frame being passed
 # into 'imshow' which displays the frames in a window called 'Live Feed'.
-def run_camera(picam):
+def frame_stream(picam):
    while True:
       frame = picam.capture_array()
-   
+
       cv.imshow('Live Feed', frame)
+      yield frame
 
       # Shows the next frame after 20ms and closes the 'Live Feed' if the
       # keybind 'd' is pressed.
       if cv.waitKey(20) & 0xFF==ord('d'):
          break
-   
