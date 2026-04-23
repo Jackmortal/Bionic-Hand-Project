d1045e99df45d2f0586dbd623bc1b76fac2d3ad1
Jack Tepper <jatep2005@gmail.com>
2026-04-23 02:12:02 -0400

    Fix: imported recognition for live gesture state feedback

--- Versions/MK0/Firmware/main.py
+++ Versions/MK0/Firmware/main.py
@@ -1,28 +1,30 @@
 from gestures import *
-from recognition import recognize_frame, settings, current_state
+from recognition import recognize_frame, settings, state
+import recognition
 import threading
 
 def main():
     servos = setup()
+    state = recognition.state
 
     # Creates a thread for recognize_frame so that new states are captured
     # from its loop, while also looping the gesture control in main.
     threading.Thread(target = recognize_frame, args=(settings,)).start()
 
     while True:
-        if current_state == 'None':
+        if state == 'None':
             break
-        elif current_state == 'Closed_Fist':
+        elif state == 'Closed_Fist':
             close_hand(servos)
-        elif current_state == 'Open_palm':
+        elif state == 'Open_palm':
             open_hand(servos)
-        elif current_state == 'Victory':
+        elif state == 'Victory':
             peace_sign(servos)
-        elif current_state == 'Thumb_Up':
+        elif state == 'Thumb_Up':
             thumbs_up(servos)
-        elif current_state == 'Pointing_Up':
+        elif state == 'Pointing_Up':
             pointing(servos)
-        elif current_state == 'ILoveYou':
+        elif state == 'ILoveYou':
             rock_on(servos)
 
 # main_guard
